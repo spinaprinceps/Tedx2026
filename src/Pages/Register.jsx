2af3c9925b8a8d2130814bf7b0c3b153cef98a8c
Shreya1083 <shreyaaitagi@gmail.com>
2025-08-09 20:08:37 +0530

    changes

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../styles/Register.css';
 
 const Register = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [formData, setFormData] = useState({
+    fullName: '',
+    usn:'',
+    phoneNumber: '',
+    email: '',
+  });
 
-export default Register
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted:', formData);
+  };
+  
+
+  return (
+    <div className="register-container">
+      <h1>JOIN THE TEDx TEAM</h1>
+      <form className="register-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="fullName"
+          placeholder="Full Name"
+          value={formData.fullName}
+          onChange={handleChange}
+          required
+        />
+         <input
+          type="text"
+          name="usn"
+          placeholder="USN"
+          value={formData.usn}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="tel"
+          name="phoneNumber"
+          placeholder="Phone Number"
+          value={formData.phoneNumber}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email ID"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Apply Now</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
